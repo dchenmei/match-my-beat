2425fcbe5f63b0e9ff647dd0224fa0fd70c1cbff
dchenmei <dchenmei@users.noreply.github.com>
2018-09-08 19:47:50 -0400

    centered popup window

--- match-my-beat/js/app.js
+++ match-my-beat/js/app.js
@@ -1,18 +1,18 @@
 (function(exports) {
 	var client_id = '9acc436bf575440aba8f1451d1266c48';
 	var redirect_uri = 'http://localhost:8000/callback.html';
-	var g_name = 'fantasy';
-	var g_tracks = '';
-
 
 	var doLogin = function(callback) {
 		var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
 			'&response_type=token' +
 			'&scope=user-top-read%20playlist-read-private%20playlist-modify-public%20playlist-modify-private' +
 			'&redirect_uri=' + encodeURIComponent(redirect_uri);
-		//localStorage.setItem('createplaylist-tracks', JSON.stringify(g_tracks));
-		localStorage.setItem('createplaylist-name', g_name);
-		var w = window.open(url, 'asdf', 'WIDTH=400,HEIGHT=500');
+
+		var width = 400;
+		var height = 500;
+		var horizontal = (screen.width / 2) - (width / 2);
+		var vertical = (screen.height / 2) - (height / 2);
+		var w = window.open(url, 'Generating BPM Playlist...', 'WIDTH=' + width + ', HEIGHT=' + height + ', TOP=' + vertical + ' ,LEFT=' + horizontal);
 	}
 
 	exports.startApp = function() {
