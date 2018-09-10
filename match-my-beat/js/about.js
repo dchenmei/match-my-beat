5e9a867c3007cd607df7a87d55899f2ba8c545ec
dchenmei <dchenmei@users.noreply.github.com>
2018-09-10 19:15:29 -0400

    Load the onclick only after document has loaded to ensure proper elements are stored

---
+++ match-my-beat/js/about.js
@@ -0,0 +1,31 @@
+$(document).ready(function()
+{
+
+// Get the box
+var info = document.getElementById('info');
+
+// Get the button that opens the info
+var btn = document.getElementById("about");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+	console.log("Fudge?");
+    info.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    info.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == info) {
+        info.style.display = "none";
+    }
+}
+
+});
